b55d2e7703ee1d9fa5a416cbff7c1a88679ef73b
02240073 <02240073.cst@rub.edu.bt>
2025-03-02 00:25:36 -0700

    My third commit

--- part 4.py
+++ part 4.py
@@ -1,4 +1,8 @@
 studentName = "Karma Deki"
 studentNumber = "02240073"
 
-print("My name is " + studentName + " and my student number is " + studentNumber)
+print("My name is " + studentName + " and my student number is " + studentNumber)\
+
+x = 3
+y = 4
+print(x + y)
